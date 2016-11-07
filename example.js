b0d67da8721dfdcf3dec8ed5f82bfea0451cbaa7
HDinger <henriette.dinger@student.hpi.uni-potsdam.de>
2016-11-07 16:02:10 +0100

    first try to keep hierarchy information

--- example.js
+++ example.js
@@ -1,40 +1,58 @@
-  function showExplorableDOM() {
-    var domElements = document.getElementById('main-content').getElementsByTagName('*');
-    for(i = 0; i < domElements.length; i++) {
-      copyElement(domElements[i]);
-    }
-    window.setTimeout(function(){
-      deleteAllCreatedElements();
-    }, 3000);
-    console.log(domElements);
+function showExplorableDOM() {
+  var initialParent = document.getElementById('main-content');
+  var childElements = document.querySelectorAll('#main-content > *');
+  
+  for(i = 0; i < childElements.length; i++) {
+    copyElement(initialParent, childElements[i]);
+    if(i > 50) 
+      break;
   }
   
-  function copyElement(element) {
-    newElement = document.createElement('div');
-    newElement.style.backgroundColor = getRandomColor();
-    newElement.style.top = element.getBoundingClientRect().top; 
-    newElement.style.left = element.getBoundingClientRect().left; 
-    newElement.style.width = element.offsetWidth;
-    newElement.style.height = element.offsetHeight;
-    newElement.style.position = 'absolute';
-    newElement.style.opacity = '0.2';
-    newElement.classList.add('created');
-    
-    document.body.appendChild(newElement);
-  }
+  window.setTimeout(function(){
+    deleteAllCreatedElements();
+  }, 10000);
+}
+
+function copyElement(parentElement, element) {
+  newElement = document.createElement('div');
   
-  function deleteAllCreatedElements() {
-    var elements = document.getElementsByClassName('created')
-    while(elements.length > 0) {
-      elements[0].remove();
+  newElement.style.borderColor = getRandomColor();
+  newElement.style.borderWidth = '2px';
+  newElement.style.borderStyle = 'solid';
+  newElement.style.top = element.getBoundingClientRect().top; 
+  newElement.style.left = element.getBoundingClientRect().left; 
+  newElement.style.width = element.offsetWidth;
+  newElement.style.height = element.offsetHeight;
+  newElement.style.position = 'absolute';
+  newElement.style.opacity = '0.4';
+  
+  newElement.innerHTML += element.tagName;
+  
+  newElement.classList.add('created');
+  parentElement.appendChild(newElement);
+  
+  if(element.children.length > 0) {
+    var childNodes = element.children;
+    for(j = 0; j < childNodes.length; j++) {
+      if(childNodes[j].parentElement == element) {
+        copyElement(newElement, childNodes[j]);
+      }
     }
   }
-  
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+}
+
+function deleteAllCreatedElements() {
+  var elements = document.getElementsByClassName('created')
+  while(elements.length > 0) {
+    elements[0].remove();
+  }
+}
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
