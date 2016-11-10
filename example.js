273b36d17b03081df57ceebe779925c3cb41beb6
HDinger <henriette.dinger@student.hpi.uni-potsdam.de>
2016-11-10 17:45:54 +0100

    SQUASHED: AUTO-COMMIT-example.html,AUTO-COMMIT-example.js,

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 function showExplorableDOM() {
-  var initialParent = document.getElementsByTagName('body')[0];
-  var childElements = document.querySelectorAll('#main-content > *');
+  initialParent = document.getElementsByTagName('body')[0];
+  childElements = document.querySelectorAll('#main-content > *');
   
-  for(i = 0; i < childElements.length; i++) {
+  for(var i = 0; i < childElements.length; i++) {
     copyElement(initialParent, childElements[i]);
     if(i > 50) 
       break;
@@ -13,18 +13,24 @@ function showExplorableDOM() {
   }, 10000);
 }
 
-function copyElement(parentElement, element) {
+function copyElement(parentElement, element, nested = false) {
   var newElement = document.createElement('div');
   
   newElement.style.borderColor = getRandomColor();
   newElement.style.borderWidth = '2px';
   newElement.style.borderStyle = 'solid';
-  newElement.style.top = element.getBoundingClientRect().top - parentElement.getBoundingClientRect().top; 
-  newElement.style.left = element.getBoundingClientRect().left - parentElement.getBoundingClientRect().left; 
+  newElement.style.top = element.getBoundingClientRect().top - parentElement.getBoundingClientRect().top;
+  newElement.style.left = element.getBoundingClientRect().left - parentElement.getBoundingClientRect().left;
   newElement.style.width = element.offsetWidth;
   newElement.style.height = element.offsetHeight;
   newElement.style.position = 'absolute';
   newElement.style.opacity = '0.6';
+  //newElement.style.display = 'none';
+  
+  //element.onmouseover = function(){ newElement.style.display = 'block' };
+  //element.onmouseout = function(){ newElement.style.display = 'none' }; 
+  
+  //newElement.onclick = function() { toggleChildren(newElement) }
   
   newElement.innerHTML += element.tagName;
   
@@ -32,27 +38,51 @@ function copyElement(parentElement, element) {
   parentElement.appendChild(newElement);
   
   if(element.children.length > 0) {
-    var childNodes = element.children;
-    for(j = 0; j < childNodes.length; j++) {
-      if(childNodes[j].parentElement == element) {
-        copyElement(newElement, childNodes[j]);
-      }
+    childNodes = getDirectChildNodes(element);
+    for(var j = 0; j < childNodes.length; j++) {
+      copyElement(newElement, childNodes[j], true);
     }
   }
 }
 
 function deleteAllCreatedElements() {
-  var elements = document.getElementsByClassName('created')
+  elements = document.getElementsByClassName('created')
   while(elements.length > 0) {
     elements[0].remove();
   }
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++ ) {
+  letters = '0123456789ABCDEF';
+  color = '#';
+  for(var i = 0; i < 6; i++ ) {
       color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
+
+function getDirectChildNodes(element) {
+  childNodes = element.children;
+  childs = [];
+  for(var i = 0; i < childNodes.length; i++){
+      childs.push(childNodes[i]);
+  }
+  
+  return childs
+}
+
+/*
+function toggleChildren(element) {
+  childNodes = getDirectChildNodes(element);
+  for(var i = 0; i < childNodes.length; i++){
+    toggle_visibility(childNodes[i])
+  }
+}
+
+function toggle_visibility(e) {
+   if(e.style.display == 'block')
+      e.style.display = 'none';
+   else
+      e.style.display = 'block';
+}
+*/
