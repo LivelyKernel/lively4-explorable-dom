132bd097926de83f9784b964f0ec5e7ed4ca5dce
Dome-GER <dominic.sauer@yahoo.de>
2016-11-14 15:20:38 +0100

    Rotate background && handle click events
    SQUASHED: AUTO-COMMIT-example.html,AUTO-COMMIT-example.js,AUTO-COMMIT-showBorder.js,AUTO-COMMIT-showBorders.js,AUTO-COMMIT-showContainer.js,

--- showContainer.js
+++ showContainer.js
@@ -1,30 +1,33 @@
-function showExplorableDOM() {
+function showContainer() {
   var initialParent = document.getElementsByTagName('body')[0];
   var childElements = document.querySelectorAll('#main-content > *');
   
   for(var i = 0; i < childElements.length; i++) {
-    copyElement(initialParent, childElements[i]);
-    if(i > 50) 
-      break;
+    if(childElements[i].tagName != 'SCRIPT') {
+      copyElement(initialParent, childElements[i]);
+    }
   }
+  
   rotateDom(45);
   document.getElementById('main-content').style.opacity = '0.3'
 }
 
-function hideExplorableDOM(){
+function hideContainer(){
   var content = document.getElementById('main-content');
   
   deleteAllCreatedElements();
   rotateDom(0);
   content.style.opacity = '1';
   
-  // Width is lost within the transformation
-  content.width = '100%';
+  // Width and height is lost within the transformation
+  content.style.width = '100%';
+  content.style.height = '100%';
 }
 
 function copyElement(parentElement, element, nested = false) {
   var newElement = document.createElement('div');
   
+  // Set style information for the new element
   newElement.style.borderColor = getRandomColor();
   newElement.style.borderWidth = '2px';
   newElement.style.borderStyle = 'solid';
@@ -35,18 +38,13 @@ function copyElement(parentElement, element, nested = false) {
   newElement.style.position = 'absolute';
   newElement.style.opacity = '1';
   newElement.style.pointerEvents = 'none';
-  //newElement.style.display = 'none';
-  
-  //element.onmouseover = function(){ newElement.style.display = 'block' };
-  //element.onmouseout = function(){ newElement.style.display = 'none' }; 
-  
-  //newElement.onclick = function() { toggleChildren(newElement) }
   
   newElement.innerHTML += element.tagName;
   
   newElement.classList.add('created');
   parentElement.appendChild(newElement);
   
+  // Keep hierarchy information by adding child elements recursively 
   if(element.children.length > 0) {
     var childNodes = getDirectChildNodes(element);
     for(var j = 0; j < childNodes.length; j++) {
@@ -65,6 +63,7 @@ function deleteAllCreatedElements() {
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
+  
   for(var i = 0; i < 6; i++ ) {
       color += letters[Math.floor(Math.random() * 16)];
   }
@@ -74,10 +73,10 @@ function getRandomColor() {
 function getDirectChildNodes(element) {
   var childNodes = element.children;
   var childs = [];
+  
   for(var i = 0; i < childNodes.length; i++){
       childs.push(childNodes[i]);
   }
-  
   return childs
 }
 
