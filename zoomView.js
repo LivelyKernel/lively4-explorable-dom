3d3fbb898d74cf365912467971634f368edb3674
Dome-GER <dominic.sauer@yahoo.de>
2016-11-21 16:25:53 +0100

    improve zoom view
    SQUASHED: AUTO-COMMIT-zoomView.js,

--- zoomView.js
+++ zoomView.js
@@ -2,22 +2,32 @@ var count = 1;
 var maxCount = 1;
 
 function showZoomView() {
+  setOpacity(0.1);
   var elements = document.getElementsByClassName('created');
   
   for(let i = 0; i < elements.length; i++){
+    // Change styling
     elements[i].style.position = 'relative';
+    elements[i].style.top = elements[i].style.top - 20;
     
     if(elements[i].children.length > 0) {
       var hierarchyLevel = countHierarchyLevel(elements[i].children);
+      increaseByHierarchyLevel(elements[i], hierarchyLevel);
       
+      elements[i].style.padding = '10px';
+      
+      // Reset counters
       maxCount = 1;
+      count = 1;
     }
   }
   
 } 
 
+// Go through DOM and count hierarchy levels
 function countHierarchyLevel(elements) {
   for(let j = 0; j < elements.length; j++){
+    // If there are children count up and go through them too
     if(elements[j].children.length > 0) {
       count++;
       if(maxCount < count) {
@@ -25,14 +35,14 @@ function countHierarchyLevel(elements) {
       }
       countHierarchyLevel(elements[j].children);
     }
+    
+    // To avoid doubled counts when both multiple siblings have children
     count--;
   }
   return maxCount;
 }
 
-function whatever(element){
-  if(elements[i].children.length > 0) {
-      elements[i].style.height += 20 + 'px';
-      elements[i].style.width += 20 + 'px';
-    }
+function increaseByHierarchyLevel(element, factor){
+  element.style.height = element.clientHeight + factor * 40 + 'px';
+  element.style.width = element.clientWidth + factor * 40 + 'px';
 }
