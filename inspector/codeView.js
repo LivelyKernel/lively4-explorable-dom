715d035739ca4a3aa03b3a67a2d5591a26a57e4f
Dome-GER <dominic.sauer@yahoo.de>
2017-01-25 11:23:51 +0100

    Show overflowing code elements on hover
    SQUASHED: AUTO-COMMIT-inspector-codeView.js,

--- inspector/codeView.js
+++ inspector/codeView.js
@@ -16,10 +16,27 @@ export default class CodeView extends ContainerView {
       elements[i].prepend(br);
       elements[i].insertAdjacentHTML('afterbegin', elements[i].dataset.content);
       let originalElement = this._inspectorContent.querySelector('#' + elements[i].dataset.id);
-      let firstCodeElement = this._createCodeElement(elements[i], originalElement);
-      let secondCodeElement = this._createCodeElement(elements[i], originalElement, false);
-      elements[i].insertBefore(firstCodeElement, elements[i].firstChild);
-      elements[i].appendChild(secondCodeElement);
+      let startTagCodeElement = this._createCodeElement(elements[i], originalElement);
+      let endTagElement = this._createCodeElement(elements[i], originalElement, false);
+      elements[i].insertBefore(startTagCodeElement, elements[i].firstChild);
+      elements[i].appendChild(endTagElement);
+      
+      if(this._isOverflowed(startTagCodeElement)) {
+        let hoverWidth = elements[i].offsetWidth + 100 + 'px';
+        startTagCodeElement.addEventListener('mouseover', function() {
+          startTagCodeElement.style.backgroundColor = 'gray';
+          startTagCodeElement.style.whiteSpace = 'normal';
+          startTagCodeElement.style.zIndex = '+1';
+          startTagCodeElement.style.width = hoverWidth;
+        });
+        let newElementWidth = this._getCodeElementWidth(elements[i]);
+        startTagCodeElement.addEventListener('mouseleave', function() {
+          startTagCodeElement.style.backgroundColor = 'initial';
+          startTagCodeElement.style.whiteSpace = 'nowrap';
+          startTagCodeElement.style.zIndex = 'auto';
+          startTagCodeElement.style.width = newElementWidth;
+        });
+      }
     }
     
     // Show all hierarchy levels by default;
@@ -62,7 +79,7 @@ export default class CodeView extends ContainerView {
         codeElement.style.top = parseFloat(createdElement.offsetTop) + parseFloat(createdElement.offsetHeight) -14 + 'px';
       }
     }
-    codeElement.style.width = parseFloat(createdElement.offsetWidth) - 7 + 'px';
+    codeElement.style.width = this._getCodeElementWidth(createdElement);
     
     return codeElement;
   }
@@ -77,5 +94,13 @@ export default class CodeView extends ContainerView {
     let text = document.createTextNode(outerHtml);
     pre.appendChild(text);
     return pre.innerHTML;
+  }
+  
+  _getCodeElementWidth(orginalElement) {
+    return parseFloat(orginalElement.offsetWidth) - 2 + 'px';
+  }
+  
+  _isOverflowed(element){
+    return element.scrollWidth > element.clientWidth;
   }
 }
